50fadf50150cab713ad4ae054a08d7b16bc4f4f9
SimoneAmoriello <103106135+SimoneAmoriello@users.noreply.github.com>
2022-05-10 16:34:28 +0200

    modifiche login.php
    aggiunta messaggi di errore

--- login.php
+++ login.php
@@ -1,25 +1,4 @@
-<?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
 
-if (isset($_POST['invio']))          /* abbiamo appena dato dati, cioe' questo script 
-									    funge da action per la form di login */
-
-  if (empty($_POST['userName']) || empty($_POST['password']))
-    echo "<p>Dati mancanti!</p>";
-  else                               // controllo dati
-    if ( ($_POST['userName']=="jonathan") && ($_POST['password']=="archer") ) { //ok
-      session_start();
-      $_SESSION['userName']=$_POST['userName'];
-      $_SESSION['dataLogin']=time();
-      $_SESSION['accessoPermesso']=1000;
-      header('Location: ST.inizio.php');    // accesso alla pagina iniziale
-      exit();
-    }
-    else {
-    echo "<p>Accesso negato!</p>";
-  }
-?>
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -35,9 +14,12 @@ if (isset($_POST['invio']))          /* abbiamo appena dato dati, cioe' questo s
 </head>
 
 <body>
-<div class="container flex-container navbar">
-          <a href="./intro.php" class="button">TORNA ALLA PAGINA INIZIALE</a>
+<div class="top">
+<div class="navbar black shadow">
+          <a href="./intro.php" class="navbar-item padding-large button">TORNA ALLA PAGINA INIZIALE</a>
         </div>
+</div>
+
 <div class="containerImmagine">
         <div class="containerBlur">
         <div class="containerCentrale">
@@ -45,9 +27,31 @@ if (isset($_POST['invio']))          /* abbiamo appena dato dati, cioe' questo s
             <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
 
                 <div class="riga">
-                    <input class="textInput" type="text" name="email" placeholder="Email" />
-                    <input class="textInput" type="text" name="password" placeholder="Password" />
-                    <input class="bottone" type="button" value="Accedi">
+                  
+                  <?php
+                        if(isset($_POST['email'])){
+                            echo "<input class=\"textInput\" type=\"text\" name=\"email\" value=\"{$_POST['email']}\" placeholder=\"Inserisci l'email\"   >";
+                        }
+                        else{
+                            echo "<input class=\"textInput\" type=\"text\" name=\"email\" placeholder=\"Inserisci l'email\" >";     
+                        }
+                        if(isset($_POST['accedi']) && $_POST['email']==""){
+                            echo "
+                                <p class=\"errorLabel\">Inserire l'email!</p> 
+                            ";
+                        }
+                    ?>
+                 
+                 <input class="textInput" type="password" name="password" placeholder="Inserisci la password" />
+                    <?php 
+                        if(isset($_POST['accedi']) && ($_POST['password']=="") && !( $_POST['email']=="")){
+                            echo "
+                                <p class=\"errorLabel\">Inserire la password!</p> 
+                            ";
+                        }
+                  ?>
+
+                    <input class="bottone" type="submit" name="accedi" value="Accedi">
                 </div>
                 <div id="registrazione">
                 <a  href="./registrazione.php">Non sei ancora iscritto? Clicca qui!</a>
