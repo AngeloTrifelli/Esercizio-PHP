572b19c19ab2036471ab555173a02a1ff113bdce
SimoneAmoriello <103106135+SimoneAmoriello@users.noreply.github.com>
2022-05-10 14:31:41 +0200

    Modifiche a registrazione.php

--- registrazione.php
+++ registrazione.php
@@ -1,3 +1,21 @@
+<?php 
+    $patternCodFisc = "/^[a-zA-Z]{6}[0-9]{2}[a-zA-Z][0-9]{2}[a-zA-Z][0-9]{3}[a-zA-Z]$/";
+
+    if (isset($_POST['continua'])){
+        if ($_POST['cognome']==""){
+        }
+        else{
+            header('Location: registrazioneFinale.php');
+            exit();
+        }
+            
+    }
+    
+?>
+
+
+
+
 
 
 
@@ -8,33 +26,161 @@
 
 <head>
     <title>Sapienza Musical Festival</title>
-    <link rel="stylesheet" href="../CSS/registrazione.css" type="text/css" />
+
+    <style>
+        <?php include "../CSS/registrazione.css" ?>
+    </style>
+
+
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@500&display=swap" />
 </head>
 
 
 <body>
-    <div class="containerImmagine"> </div>
-
-
-    <div class="containerCentrale">
-            <h1>REGISTRAZIONE UTENTE</h1>
-            <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
-
-                <div class="riga">
-                    <input type="text" name="nome" placeholder="Inserisci il nome" />
-                    <input type="text" name="cognome" placeholder="Inserisci il cognome" />
-                </div>
-                
-                <div class="riga">
-                    <input type="text" name="codFisc" placeholder="Inserisci il codice fiscale" />
-                    <input type="date" name="dataNascita" />
-                </div>
-                
-                
-              
-            </form>
+    <div class="top">
+        <div class="navbar black shadow">
+            <a href="./intro.php" class="navbar-item padding-large button">HOME</a> 
+            <a href="#" class="navbar-item padding-large button">BAND</a> 
+            <a href="#" id=loginButton class="navbar-item padding-large button">LOGIN</a>
+     
+        </div>
     </div>
+
+
+
+    
+    <div class="containerImmagine"> 
+
+        <div class="containerBlur">
+
+            <div class="containerCentrale">
+
+                <h1>REGISTRAZIONE UTENTE</h1>
+        
+                <div class="tabella">
+                    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+
+                        <div class="riga">
+                            <div class=containerColumn>
+                                <?php 
+                                    if(isset($_POST['nome'])){
+                                        echo "<input class=\"textInput\" type=\"text\" name=\"nome\" value=\"{$_POST['nome']}\" placeholder=\"Inserisci il nome\"   >";
+                                    }
+                                    else{
+                                        echo "<input class=\"textInput\" type=\"text\" name=\"nome\" placeholder=\"Inserisci il nome\" >";     
+                                    }
+                                    if(isset($_POST['continua']) && $_POST['nome']==""){
+                                        echo "
+                                            <p class=\"errorLabel\">Inserire il nome!</p> 
+                                        ";
+                                    }
+                                ?>
+                            </div>
+                            
+                            <div class="containerColumn">
+                                <?php 
+                                    if(isset($_POST['cognome'])){
+                                        echo "<input class=\"textInput\" type=\"text\" name=\"cognome\" value=\"{$_POST['cognome']}\" placeholder=\"Inserisci il cognome\" >";
+                                    }
+                                    else{
+                                        echo "<input class=\"textInput\" type=\"text\" name=\"cognome\" placeholder=\"Inserisci il cognome\" >";     
+                                    }
+                                    if(isset($_POST['continua']) && $_POST['cognome']==""){
+                                        echo "
+                                            <p class=\"errorLabel\">Inserire il cognome!</p> 
+                                        ";
+                                    }
+                                ?>
+                            </div>
+                            
+                        </div>
+
+                        <div class="riga">
+                            <div class="containerColumn">                               
+                                <?php 
+                                    if(isset($_POST['codFisc'])){
+                                        echo "<input class=\"textInput\" id=\"codiceFiscale\" type=\"text\" name=\"codFisc\" value=\"{$_POST['codFisc']}\" placeholder=\"Inserisci il codice fiscale\"   >";
+                                    }
+                                    else{
+                                        echo "<input class=\"textInput\" id=\"codiceFiscale\" type=\"text\" name=\"codFisc\" placeholder=\"Inserisci il codice fiscale\" >";     
+                                    }
+                                    if(isset($_POST['continua']) && $_POST['codFisc']==""){
+                                        echo "
+                                            <p class=\"errorLabel\">Inserire il codice fiscale!</p> 
+                                        ";
+                                    }
+                                    elseif (isset($_POST['continua']) && (!preg_match($patternCodFisc, $_POST['codFisc'])) ){
+                                        echo "
+                                            <p class=\"errorLabel\">Il codice fiscale inserito non è valido!</p>
+                                        ";
+                                    }
+                                ?>
+                            </div>
+                            
+
+                            <div class=containerData>
+                                <h3> Inserisci la data di nascita:</h3>
+                                <?php 
+                                    if(isset($_POST['dataNascita'])){
+                                        echo "<input class=\"dateInput\" type=\"date\" name=\"dataNascita\" value=\"{$_POST['dataNascita']}\" >";
+                                    }
+                                    else{
+                                        echo "<input class=\"dateInput\" type=\"date\" name=\"dataNascita\">";     
+                                    }
+                                    if(isset($_POST['continua']) && $_POST['dataNascita']==""){
+                                        echo "
+                                            <p class=\"errorLabel\">Inserire la data di nascita!</p> 
+                                        ";
+                                    }
+                                ?>
+                            </div>
+                            
+                        
+                        </div>  
+
+                        <div class="riga">
+                            <div class="containerColumn">
+                                <?php 
+                                    if(isset($_POST['domicilio'])){
+                                        echo "<input class=\"textInput\" type=\"text\" name=\"domicilio\" value=\"{$_POST['domicilio']}\" placeholder=\"Inserisci la via\" >";
+                                    }
+                                    else{
+                                        echo "<input class=\"textInput\" type=\"text\" name=\"domicilio\" placeholder=\"Inserisci la via\" >";     
+                                    }
+                                    if(isset($_POST['continua']) && ( $_POST['domicilio']=="" || $_POST['numCiv']=="")){
+                                        echo "
+                                            <p class=\"errorLabel\">Inserire la via e/o il numero civico!</p> 
+                                        ";
+                                    }
+                                ?>
+                            </div>
+                            
+                            <?php
+                                if(isset($_POST['numCiv'])){
+                                    echo "<input id=\"numeroCivicoInput\" type=\"number\" name=\"numCiv\" value=\"{$_POST['numCiv']}\" placeholder=\"N°\"   >";
+                                }
+                                else{
+                                    echo "<input id=\"numeroCivicoInput\" type=\"number\" name=\"numCiv\" placeholder=\"N°\" >";     
+                                }
+                            ?>
+                        </div>
+
+                        <div class="riga">
+                            <input type="submit" class="continuaButton black button" name="continua" value="Continua">
+                        </div>
+                    </form>
+
+                </div>
+                    
+            </div>
+
+        </div>
+    </div>
+    
+
+
+    
  
    
 
