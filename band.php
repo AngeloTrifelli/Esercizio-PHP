bac55ebf0583dc2766694c0c680a3509893721a4
AngeloTrifelli <103106138+AngeloTrifelli@users.noreply.github.com>
2022-05-18 13:59:44 +0200

    Modifiche a band.php
    Implementata la procedura di prenotazione di un biglietto

--- band.php
+++ band.php
@@ -3,15 +3,26 @@
     session_start();
 
 
-    $queryNumeroArtisti = "SELECT COUNT(*) FROM artista";
-    if($resultQ = mysqli_query($mysqliConnection, $queryNumeroArtisti)){
-        $numArtisti = (int)mysqli_fetch_array($resultQ);
-    }
 
     $queryArtisti = "SELECT * FROM artista";
-    if ($resultQ2 = mysqli_query($mysqliConnection,$queryArtisti)){
-    }
+    $resultQ = mysqli_query($mysqliConnection,$queryArtisti);
 
+
+    $queryIDassociazioni = "SELECT id FROM associato";
+    $resultQueryID = mysqli_query($mysqliConnection,$queryIDassociazioni);
+
+    while ($idAssociazione = mysqli_fetch_array($resultQueryID)){
+        if (isset($_POST[$idAssociazione[0]])){
+            if(isset($_SESSION['emailUtente']) && isset($_SESSION['passwordUtente']) ){
+                $_SESSION['associazioneScelta'] = $idAssociazione[0];
+                $_SESSION['accessoPermesso'] = True;
+                header("Location: confermaPrenotazione.php");
+            }
+            else{
+                header("Location: login.php");
+            }
+        }
+    }
     
 
 
@@ -72,8 +83,7 @@
 
     
     <?php 
-        for($i=0;$i<=$numArtisti;$i++){ 
-            $artista = mysqli_fetch_array($resultQ2);
+        while($artista = mysqli_fetch_array($resultQ)){ 
     ?>
         <div class="band paragraph">
             <h1><?php echo $artista['nome'];?></h1>
@@ -99,23 +109,48 @@
                             echo $giorno."-".$mese."-".$anno." ".$ora;
                         ?>
                     </td>
+
+                    <?php 
+                    $queryBiglietti = "SELECT * FROM associato WHERE id_artista=\"{$artista['nome']}\" ";
+                    $resultQ2 = mysqli_query($mysqliConnection,$queryBiglietti);
+
+                   
+                    while($biglietto = mysqli_fetch_array($resultQ2)){
+                        $queryPrezzo = "SELECT prezzo FROM biglietto WHERE tipo=\"{$biglietto['id_biglietto']}\" ";
+                        $resultQ3 = mysqli_query($mysqliConnection,$queryPrezzo);
+                        $prezzo = mysqli_fetch_array($resultQ3);
+
+                        $queryNumPrenotazioni = "SELECT COUNT(*) FROM prenota WHERE id_associazione = {$biglietto['id']} ";
+                        $resultQ4 = mysqli_query($mysqliConnection,$queryNumPrenotazioni);
+                        $numPrenotazioni = mysqli_fetch_array($resultQ4);
+
+                        $postiDisponibili = $biglietto['numPosti'] - $numPrenotazioni[0];
+
+                    ?>
+
                     <td>
                         <br />
-                        <span class="ticket"> BIGLIETTO BASIC 40&euro;</span>
+                        <span class="ticket"> BIGLIETTO <?php echo $biglietto['id_biglietto']." ".$prezzo[0];?> &euro;</span>
                         <br />
-                        <input class="bottone" type="button" value="Acquista ora">
+                        <?php 
+                            if($postiDisponibili!=0){       // Mostro il bottone solamente se ho dei posti disponibili
+                        ?>
+                            <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+                                <input class="bottone" type="submit"  name="<?php echo $biglietto['id'];?>" value="Acquista ora">    
+                            </form>   
+                        <?php
+                            }
+                        ?>
+
                         <br />
-                        <span class="posti">Posti disponibili:250</span>
+                        <span class="posti">Posti disponibili:<?php echo $postiDisponibili;?> </span>
     
                     </td>
-                    <td>
-                        <br />
-                        <span class="ticket">BIGLIETTO PREMIUM 70&euro;</span>
-                        <br />
-                        <input class="bottone" type="button" value="Acquista ora">
-                        <br />
-                        <span class="posti">Posti disponibili:250</span>
-                    </td>
+
+                    <?php
+                    }
+                    ?>
+    
                 </tr>
             </table>
         </div>
