1561037e73129001d9ecbc2f479d107a07238bf1
AngeloTrifelli <103106138+AngeloTrifelli@users.noreply.github.com>
2022-05-19 14:08:00 +0200

    Update install.php

--- install.php
+++ install.php
@@ -115,7 +115,8 @@ $InserimentoDatiArtisti = "INSERT INTO artista VALUES
 Sono tra i gruppi musicali con più vendite della storia, avendo venduto più di 85 milioni di dischi in tutto il mondo. Inoltre la rivista Rolling Stone, li ha inseriti nella lista New Immortals, elenco degli artisti definiti le nuove leggende della storia della musica.
 Nel 1994, grazie al successo del loro terzo album Dookie, il quale, con 10 dischi di platino e uno di diamante, ha venduto più di 10 milioni di copie solo negli Stati Uniti e 15 in tutto il mondo, sono stati annoverati, insieme a gruppi come Offspring e Rancid, tra le band che hanno riportato il punk rock nella musica mainstream.' , '2022-06-16 18:00'),
 ('Pink Floyd', 'https://favolosi60com.files.wordpress.com/2021/04/pink-floyd-1973-32d763d9-8deb-4cd8-a287-92ae0de7df3a.jpg', 'I Pink Floyd sono stati un gruppo rock britannico, fondato a Londra nel 1965. <br />Con la scrittura di Roger Waters e le fantasche melodie di David Guilmoure hanno rappresentato una vera e propria epoca del rock e del progressive. 
-<br />Al Sapienza Musical Festival potrai ritornare indietro nel tempo e essere presente al ritorno di questa band leggendaria.' , '2022-05-26 21:00');";
+<br />Al Sapienza Musical Festival potrai ritornare indietro nel tempo e essere presente al ritorno di questa band leggendaria.' , '2022-05-26 21:00'),
+('Blanco', 'https://m.media-amazon.com/images/I/A1S3OwNU+NL._AC_SL1500_.jpg', 'Blanco, pseudonimo di Riccardo Fabbriconi (Brescia, 10 febbraio 2003), è un cantautore italiano.<br /> È salito alla ribalta nel 2021 con i successi La canzone nostra e Mi fai impazzire, arrivati al vertice della Top Singoli italiana. Ha in seguito pubblicato il suo primo album Blu celeste, certificato quadruplo platino e trainato dai singoli Notti in bianco, Paraocchi. Nel 2022 ha vinto il 72º Festival di Sanremo con il brano Brividi, in duetto con Mahmood, la cui canzone è entrata in classifiche di vari Paesi e con la quale ha rappresentato l\'Italia all\'Eurovision Song Contest 2022.', '2022-07-28 21:00');";
 
 
 try{
@@ -133,9 +134,14 @@ catch(mysqli_sql_exception $exception){
 
 
 
+
+
+
+
 $InserimentoDatiBiglietti ="INSERT INTO biglietto VALUES
 ('BASIC', '40.0'),
-('PREMIUM','70.0');";
+('DELUXE', '60.0'),
+('PREMIUM','90.0');";
 
 try{
     if($resultQ = mysqli_query($mysqliConnection, $InserimentoDatiBiglietti)){
@@ -155,7 +161,10 @@ catch(mysqli_sql_exception $exception){
 $InserimentoDatiAssociazioni ="INSERT INTO associato (id_biglietto, id_artista, numPosti) VALUES
 ('BASIC', 'Green Day', 250),
 ('PREMIUM', 'Green Day', 150),
-('BASIC', 'Pink Floyd', 200);";
+('BASIC', 'Pink Floyd', 200),
+('BASIC', 'Blanco' , 300),
+('DELUXE', 'Blanco', 200),
+('PREMIUM', 'Blanco', 100);";
 
 try{
     if($resultQ = mysqli_query($mysqliConnection, $InserimentoDatiAssociazioni)){
